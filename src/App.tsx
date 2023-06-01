cfa69bbb7623a896476fc397c7384dc64269bf52
vinithecsar <cesar.v@escolar.ifrn.edu.br>
2023-06-01 10:02:47 -0300

    componentes organizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,50 @@
-import { useState } from 'react'
 import './App.css'
 
-const Index = () => {
+const ItensDaLista = () => {
   return (
-    <><div className="container">
-      <div className="row">
-        <div className="col-md-12">
-          <div className="card card-white">
-            <div className="card-body">
-              <form action="javascript:void(0);">
-                <input
-                  type="text"
-                  className="form-control add-task"
-                  placeholder="New Task..." />
-              </form>
-              <ul className="nav nav-pills todo-nav">
-                <li
-                  role="presentation"
-                  className="nav-item all-task active"
-                >
-                  <a href="#" className="nav-link">All</a>
-                </li>
-                <li
-                  role="presentation"
-                  className="nav-item active-task"
-                >
-                  <a href="#" className="nav-link">Active</a>
-                </li>
-                <li
-                  role="presentation"
-                  className="nav-item completed-task"
-                >
-                  <a href="#" className="nav-link">Completed</a>
-                </li>
-              </ul>
-              <div className="todo-list">
-                <div className="todo-item">
-                  <div className="checker">
-                    <span
-                    ><input type="checkbox" /></span>
-                  </div>
-                  <span>Create theme</span>
-                  <a
-                    href="javascript:void(0);"
-                    className="float-right remove-todo-item"
-                  ><i className="icon-close"></i></a>
-                </div>
-                <div className="todo-item">
-                  <div className="checker">
-                    <span
-                    ><input type="checkbox" /></span>
-                  </div>
-                  <span>Work on wordpress</span>
-                  <a
-                    href="javascript:void(0);"
-                    className="float-right remove-todo-item"
-                  ><i className="icon-close"></i></a>
-                </div>
-                <div className="todo-item">
-                  <div className="checker">
-                    <span
-                    ><input type="checkbox" /></span>
-                  </div>
-                  <span>Organize office main department</span>
-                  <a
-                    href="javascript:void(0);"
-                    className="float-right remove-todo-item"
-                  ><i className="icon-close"></i></a>
-                </div>
-                <div className="todo-item">
-                  <div className="checker">
-                    <span><input type="checkbox" /></span>
-                  </div>
-                  <span>Error solve in HTML template</span>
-                  <a
-                    href="javascript:void(0);"
-                    className="float-right remove-todo-item"
-                  ><i className="icon-close"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+  <ul className="nav nav-pills todo-nav">
+    <li role="presentation" className="nav-item all-task active">
+      <a href="#" className="nav-link">All</a>
+    </li>
+    <li role="presentation" className="nav-item active-task">
+      <a href="#" className="nav-link">Active</a>
+    </li>
+    <li role="presentation" className="nav-item completed-task">
+      <a href="#" className="nav-link">Completed</a>
+    </li>
+  </ul>
+  )
+}
+
+const BarraPesquisa = () => {
+  return (
+    <form action="javascript:void(0);">
+    <input
+      type="text"
+      className="form-control add-task"
+      placeholder="New Task..." />
+  </form>
+  )
+}
+
+const ListaToDo = () => {
+  let lista = [
+    "Create theme",
+    "Work on wordpress",
+    "Organize office main department",
+    "Error solve in HTML template"
+  ];
+  return (
+    <>
+    {lista.map((nome, index) => 
+    <div className="todo-item">
+    <div className="checker">
+      <span><input type="checkbox" /></span>
     </div>
+    <span> {nome}</span>
+    <a href="javascript:void(0);" className="float-right remove-todo-item"><i className="icon-close"></i></a>
+    </div>
+    )}
     </>
   )
 }
@@ -91,7 +52,21 @@ const Index = () => {
 function App() {
   return (
     <>
-    <Index/>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-white">
+            <div className="card-body">
+              <BarraPesquisa/>
+              <ItensDaLista/>
+              <div className="todo-list">
+              <ListaToDo/>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
